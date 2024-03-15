4396936bd05fa82ec6b0fbe90b5ebc80c75d9034
EnyByy <rmeliud@gmail.com>
2024-03-15 12:59:44 -0500

    1.62

--- PROGRAMS/INTERFACE/04_1chatbot.py
+++ PROGRAMS/INTERFACE/04_1chatbot.py
@@ -3,7 +3,7 @@ from tkinter import scrolledtext
 from openai import OpenAI
 
 # INGRESAR APIKEY
-# INGRESAR API KEY
+api_key = ''
 client = OpenAI(api_key=api_key)
 
 input_text = "Eres un robot erudito"
